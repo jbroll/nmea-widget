67c41aeeb1deed0408b471722b6fdbc66daf0d6d
John Roll <john@rkroll.com>
2025-01-12 20:38:40 -0500

    deploy paths

---
+++ examples/nmea-demo/vite.config.ts
@@ -0,0 +1,18 @@
+import { defineConfig } from 'vite'
+import preact from '@preact/preset-vite'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [preact()],
+  base: '/nmea-widgets/',  // GitHub Pages repository name
+  build: {
+    assetsDir: 'assets',
+    rollupOptions: {
+      output: {
+        assetFileNames: 'assets/[name]-[hash][extname]',
+        chunkFileNames: 'assets/[name]-[hash].js',
+        entryFileNames: 'assets/[name]-[hash].js',
+      }
+    }
+  }
+})
