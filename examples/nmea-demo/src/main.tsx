823ddb50323cd39b9674a07364726b384d91037b
John Roll <john@rkroll.com>
2025-01-14 09:14:30 -0500

    push useNMEA down into NMEADisplay

--- examples/nmea-demo/src/main.tsx
+++ examples/nmea-demo/src/main.tsx
@@ -1,30 +1,5 @@
 import { render } from 'preact';
 import './globals.css';
-import { NMEADisplay, useNMEA } from '@jbroll/nmea-widgets';
+import { NMEADisplay } from '@jbroll/nmea-widgets';
 
-function App() {
-  const { 
-    serialData,
-    processedData,
-    isConnected,
-    connect,
-    disconnect,
-    setFilter,
-    isSupported
-  } = useNMEA();
-
-  return (
-    <NMEADisplay
-      serialData={serialData}
-      processedData={processedData}
-      onConnect={connect}
-      onDisconnect={disconnect}
-      onFilterChange={setFilter}
-      isConnected={isConnected}
-      isSupported={isSupported}
-    />
-  );
-}
-
-// Initial render
-render(<App />, document.getElementById('app')!);
+render(<NMEADisplay />, document.getElementById('app')!);
